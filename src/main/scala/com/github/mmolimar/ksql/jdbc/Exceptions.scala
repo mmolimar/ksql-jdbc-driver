58d156911fa7d9e3f1612991a7c88b1a9fd5585c
Mario Molina <mmolimar@gmail.com>
2019-01-27 14:34:48 -0600

    New KSQL result set error

--- src/main/scala/com/github/mmolimar/ksql/jdbc/Exceptions.scala
+++ src/main/scala/com/github/mmolimar/ksql/jdbc/Exceptions.scala
@@ -11,11 +11,11 @@ sealed trait KsqlException {
 }
 
 case class InvalidUrl(url: String, override val cause: Throwable = None.orNull) extends KsqlException {
-  override def message = s"URL with value ${url} is not valid. It must match de regex ${KsqlDriver.urlRegex}"
+  override def message = s"URL with value ${url} is not valid. It must match de regex ${KsqlDriver.urlRegex}."
 }
 
 case class CannotConnect(url: String, msg: String, override val cause: Throwable = None.orNull) extends KsqlException {
-  override def message = s"Cannot connect to this URL ${url}. Error message: ${msg}"
+  override def message = s"Cannot connect to this URL ${url}. Error message: ${msg}."
 }
 
 case class InvalidProperty(name: String, override val cause: Throwable = None.orNull) extends KsqlException {
@@ -26,28 +26,31 @@ case class NotSupported(feature: String, override val cause: Throwable = None.or
   override val message = s"Feature not supported: $feature."
 }
 
-case class KsqlQueryError(override val message: String = "Error executing query",
+case class KsqlQueryError(override val message: String = "Error executing query.",
                           override val cause: Throwable = None.orNull) extends KsqlException
 
-case class KsqlCommandError(override val message: String = "Error executing command",
+case class KsqlCommandError(override val message: String = "Error executing command.",
                             override val cause: Throwable = None.orNull) extends KsqlException
 
-case class KsqlEntityListError(override val message: String = "Invalid KSQL entity list",
+case class KsqlEntityListError(override val message: String = "Invalid KSQL entity list.",
+                               override val cause: Throwable = None.orNull) extends KsqlException
+
+case class InvalidColumn(override val message: String = "Invalid column.",
                          override val cause: Throwable = None.orNull) extends KsqlException
 
-case class InvalidColumn(override val message: String = "Invalid column",
-                         override val cause: Throwable = None.orNull) extends KsqlException
-
-case class EmptyRow(override val message: String = "Current row is empty",
+case class EmptyRow(override val message: String = "Current row is empty.",
                     override val cause: Throwable = None.orNull) extends KsqlException
 
-case class UnknownTableType(override val message: String = "Table type does not exist",
-                            override val cause: Throwable = None.orNull) extends KsqlException
-
-case class UnknownCatalog(override val message: String = "Catalog does not exist",
+case class ResultSetError(override val message: String = "Error accessing to the result set.",
                           override val cause: Throwable = None.orNull) extends KsqlException
 
-case class UnknownSchema(override val message: String = "Schema does not exist",
+case class UnknownTableType(override val message: String = "Table type does not exist.",
+                            override val cause: Throwable = None.orNull) extends KsqlException
+
+case class UnknownCatalog(override val message: String = "Catalog does not exist.",
+                          override val cause: Throwable = None.orNull) extends KsqlException
+
+case class UnknownSchema(override val message: String = "Schema does not exist.",
                          override val cause: Throwable = None.orNull) extends KsqlException
 
 object Exceptions {
