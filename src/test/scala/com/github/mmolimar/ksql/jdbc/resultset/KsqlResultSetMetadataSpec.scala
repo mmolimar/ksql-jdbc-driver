2e8857cad2ef4cf40e8a37399648aed0737a0f02
Mario Molina <mmolimar@gmail.com>
2019-02-02 20:03:51 -0600

    Unit tests for result set metadata

--- src/test/scala/com/github/mmolimar/ksql/jdbc/resultset/KsqlResultSetMetadataSpec.scala
+++ src/test/scala/com/github/mmolimar/ksql/jdbc/resultset/KsqlResultSetMetadataSpec.scala
@@ -1,47 +1,35 @@
 package com.github.mmolimar.ksql.jdbc.resultset
 
-import java.sql.{SQLException, SQLFeatureNotSupportedException}
-import java.util
-import java.util.Collections
+import java.sql.{ResultSetMetaData, SQLFeatureNotSupportedException, Types}
 
+import com.github.mmolimar.ksql.jdbc.HeaderField
 import com.github.mmolimar.ksql.jdbc.utils.TestUtils._
-import io.confluent.ksql.query.QueryId
-import io.confluent.ksql.rest.entity.SchemaInfo.Type
-import io.confluent.ksql.rest.entity.{EntityQueryId, FieldInfo, QueryDescription, SchemaInfo}
+import io.confluent.ksql.rest.entity.SchemaInfo.{Type => KsqlType}
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{Matchers, OneInstancePerTest, WordSpec}
 
 class KsqlResultSetMetadataSpec extends WordSpec with Matchers with MockFactory with OneInstancePerTest {
 
-  val implementedMethods = Seq("getSchemaName", "getCatalogName", "getColumnLabel", "getColumnName",
-    "getColumnTypeName", "getColumnClassName", "isCaseSensitive", "getTableName", "getColumnType",
-    "getColumnCount", "getPrecision", "getScale")
+  val implementedMethods = Seq("getColumnLabel", "getColumnName",
+    "getColumnTypeName", "getColumnClassName", "isCaseSensitive", "getColumnType",
+    "getColumnCount", "getPrecision", "getScale", "getColumnDisplaySize", "isNullable")
 
   "A KsqlResultSetMetadata" when {
 
     "validating specs" should {
 
-      val fields = util.Arrays.asList(
-        new FieldInfo("field1", new SchemaInfo(Type.INTEGER, None.orNull, None.orNull)),
-        new FieldInfo("field2", new SchemaInfo(Type.BIGINT, None.orNull, None.orNull)),
-        new FieldInfo("field3", new SchemaInfo(Type.DOUBLE, None.orNull, None.orNull)),
-        new FieldInfo("field4", new SchemaInfo(Type.BOOLEAN, None.orNull, None.orNull)),
-        new FieldInfo("field5", new SchemaInfo(Type.STRING, None.orNull, None.orNull)),
-        new FieldInfo("field6", new SchemaInfo(Type.MAP, None.orNull, None.orNull)),
-        new FieldInfo("field7", new SchemaInfo(Type.ARRAY, None.orNull, None.orNull)),
-        new FieldInfo("field8", new SchemaInfo(Type.STRUCT, None.orNull, None.orNull))
-      )
-      val queryDesc = new QueryDescription(
-        new EntityQueryId(new QueryId("testquery")),
-        "select * from text;",
-        fields,
-        Collections.singleton("TEST"),
-        Collections.emptySet[String],
-        "test",
-        "executionPlan",
-        Collections.emptyMap[String, AnyRef]
-      )
-      val resultSet = new KsqlResultSetMetadata(queryDesc)
+      val resultSet = new KsqlResultSetMetadata(
+        List(
+          HeaderField("field1", Types.INTEGER, 16),
+          HeaderField("field2", Types.BIGINT, 16),
+          HeaderField("field3", Types.DOUBLE, 16),
+          HeaderField("field4", Types.BOOLEAN, 16),
+          HeaderField("field5", Types.VARCHAR, 16),
+          HeaderField("field6", Types.JAVA_OBJECT, 16),
+          HeaderField("field7", Types.ARRAY, 16),
+          HeaderField("field8", Types.STRUCT, 16),
+          HeaderField("field9", -999, 16)
+        ))
 
       "throw not supported exception if not supported" in {
 
@@ -55,45 +43,81 @@ class KsqlResultSetMetadataSpec extends WordSpec with Matchers with MockFactory 
 
       "work if implemented" in {
 
-        resultSet.getCatalogName(1) should be("TEST")
-        resultSet.getSchemaName(2) should be("BIGINT")
-        resultSet.getColumnLabel(3) should be("field3")
+        resultSet.getColumnLabel(3) should be("FIELD3")
         resultSet.getColumnName(3) should be("field3")
         resultSet.getColumnTypeName(3) should be("DOUBLE")
 
         resultSet.getColumnClassName(1) should be("java.lang.Integer")
         resultSet.getColumnType(1) should be(java.sql.Types.INTEGER)
+        resultSet.getColumnTypeName(1) should be(KsqlType.INTEGER.name)
+        resultSet.getColumnDisplaySize(1) should be(16)
+
         resultSet.getColumnClassName(2) should be("java.lang.Long")
         resultSet.getColumnType(2) should be(java.sql.Types.BIGINT)
+        resultSet.getColumnTypeName(2) should be(KsqlType.BIGINT.name)
+        resultSet.getColumnDisplaySize(2) should be(16)
+
         resultSet.getColumnClassName(3) should be("java.lang.Double")
         resultSet.getColumnType(3) should be(java.sql.Types.DOUBLE)
+        resultSet.getColumnTypeName(3) should be(KsqlType.DOUBLE.name)
+        resultSet.getColumnDisplaySize(3) should be(16)
+
         resultSet.getColumnClassName(4) should be("java.lang.Boolean")
         resultSet.getColumnType(4) should be(java.sql.Types.BOOLEAN)
+        resultSet.getColumnTypeName(4) should be(KsqlType.BOOLEAN.name)
+        resultSet.getColumnDisplaySize(4) should be(5)
+
         resultSet.getColumnClassName(5) should be("java.lang.String")
         resultSet.getColumnType(5) should be(java.sql.Types.VARCHAR)
+        resultSet.getColumnTypeName(5) should be(KsqlType.STRING.name)
+        resultSet.getColumnDisplaySize(5) should be(64)
+
         resultSet.getColumnClassName(6) should be("java.util.Map")
         resultSet.getColumnType(6) should be(java.sql.Types.JAVA_OBJECT)
+        resultSet.getColumnTypeName(6) should be(KsqlType.MAP.name)
+        resultSet.getColumnDisplaySize(6) should be(64)
+
         resultSet.getColumnClassName(7) should be("java.sql.Array")
         resultSet.getColumnType(7) should be(java.sql.Types.ARRAY)
+        resultSet.getColumnTypeName(7) should be(KsqlType.ARRAY.name)
+        resultSet.getColumnDisplaySize(7) should be(64)
+
         resultSet.getColumnClassName(8) should be("java.sql.Struct")
         resultSet.getColumnType(8) should be(java.sql.Types.STRUCT)
+        resultSet.getColumnTypeName(8) should be(KsqlType.STRUCT.name)
+        resultSet.getColumnDisplaySize(8) should be(64)
+
+        resultSet.getColumnClassName(9) should be("java.lang.String")
+        resultSet.getColumnType(9) should be(-999)
+        resultSet.getColumnTypeName(9) should be(KsqlType.STRING.name)
+        resultSet.getColumnDisplaySize(9) should be(64)
 
         resultSet.isCaseSensitive(2) should be(false)
         resultSet.isCaseSensitive(5) should be(true)
-        resultSet.getTableName(3) should be("test")
         resultSet.getColumnType(3) should be(java.sql.Types.DOUBLE)
-        resultSet.getColumnCount should be(8)
+        resultSet.getColumnCount should be(9)
         resultSet.getPrecision(3) should be(-1)
         resultSet.getPrecision(2) should be(0)
         resultSet.getScale(3) should be(-1)
         resultSet.getScale(4) should be(0)
+        resultSet.isNullable(1) should be(ResultSetMetaData.columnNullableUnknown)
+      }
+    }
+  }
 
-        assertThrows[SQLException] {
-          resultSet.getSchemaName(0)
-        }
-        assertThrows[SQLException] {
-          resultSet.getSchemaName(9)
-        }
+  "A ResultSetMetadataNotSupported" when {
+
+    "validating specs" should {
+
+      "throw not supported exception if not supported" in {
+
+        val resultSet = new ResultSetMetadataNotSupported
+        reflectMethods[ResultSetMetadataNotSupported](Seq.empty, false, resultSet)
+          .foreach(method => {
+            assertThrows[SQLFeatureNotSupportedException] {
+              method()
+            }
+          })
       }
     }
   }
